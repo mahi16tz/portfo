6dbd831c093e60d41ae3899139481df4f1cad17d
Mahlet <mahletalmi@gmail.com>
2022-08-19 19:21:49 +0300

    updated the server.py file

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import email
-from inspect import _SourceObjectType
+# from inspect import _SourceObjectType
 from msilib.schema import Component
 from flask import Flask,render_template,url_for,request,redirect
 import csv
@@ -11,23 +11,25 @@ def my_home(page_name):
     return render_template(page_name)
 
 def write_to_file(data):
-    with open('database.txt',mode='a')as database:
+    with open('database.txt',mode='a') as database:
         email=data['email']
         subject=data['subject']
         message=data['message']
-        file=database.wirte(f'\n{email},{subject},{message}')
+        database.write(f'\n{email},{subject},{message}')
 def write_to_csv(data):
-    with open ('database.csv',mode='a')as database2:
-        email=data['email']
-        subject=data['subject']
-        message=data['message']
-        csv_writer=csv.writer(database2,delimiter=',',newline='',quotechar='"',quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email,subject,message])
-@app.route('/submit_form ', methods=['POST', 'GET'])
+         with open ('database.csv',mode='a',newline="") as database2:
+            email=data['email']
+            subject=data['subject']
+            message=data['message']
+            csv_writer=csv.writer(database2,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(["Email", "Subject", "Message"])
+            csv_writer.writerow([email,subject,message])
+@app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method=='POST':
         try:
             data=request.form.to_dict()
+         
             write_to_file(data)
             write_to_csv(data)
             return redirect('/thankyou.html')
